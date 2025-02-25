4bae0ce9f3ef17cda6eb6442c81cec94a0c327db
MoneyMan82 <antti.nokso@gmail.com>
2025-02-25 18:18:20 +0200

    Viides Commit

--- public/script.js
+++ public/script.js
@@ -1,5 +1,7 @@
 document.getElementById("new-game-btn").addEventListener('click', startGame);
 
 function startGame(){
-    console.log("works")
+    console.log("works");
+    document.getElementById("intro-screen").style.display='none'
+    document.getElementById("game-screen").style.display='block'
 }
