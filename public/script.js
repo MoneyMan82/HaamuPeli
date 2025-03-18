96c0b35ee63c3f60ac9fa5a71e68a4db15b44d33
MoneyMan82 <antti.nokso@gmail.com>
2025-03-18 18:17:24 +0200

    Kuudes Commit

--- public/script.js
+++ public/script.js
@@ -7,7 +7,7 @@ function startGame(){
     console.log("works");
     document.getElementById("intro-screen").style.display='none'
     document.getElementById("game-screen").style.display='block'
-    generateRandomBoard();
+    board = generateRandomBoard();
 }
 function generateRandomBoard(){
     const newBoard = Array.from({length: BOARD_SIZE}, ()=> Array(BOARD_SIZE).fill(' '));
@@ -20,4 +20,23 @@ function generateRandomBoard(){
             }
         }
     }
+    return newBoard
+}
+
+function drawBoard(board){
+    const gameBoard = document.getElementById("game-board");
+
+    gameBoard.style.gridTemplateColumns = `repeat(${BOARD_SIZE}, 1fr)`;
+
+    for(let y = 0; y < BOARD_SIZE; y++){
+        for(let x = 0; x < BOARD_SIZE; x++){
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            if(board[y][x]=='W'){
+                cell.classList.add('wall');
+            }
+        }
+    }
+
+
 }
