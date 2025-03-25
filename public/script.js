f6d2f7b03acecc3cd888d844d4cb339ed5e9ca2a
MoneyMan82 <antti.nokso@gmail.com>
2025-03-25 18:14:26 +0200

    7. Commit

--- public/script.js
+++ public/script.js
@@ -1,13 +1,25 @@
 let BOARD_SIZE = 12;
+const cellSize = calculateCellSize();
 let board;
 
 document.getElementById("new-game-btn").addEventListener('click', startGame);
+
+function calculateCellSize() {
+    // Otetaan talteen pienempi luku ikkunan leveydestä ja korkeudesta
+    const screenSize = Math.min(window.innerWidth, window.innerHeight);
+    // Tehdään pelilaudasta hieman tätä pienempi, jotta jää pienet reunat
+    const gameBoardSize = 0.95 * screenSize;
+    // Laudan koko jaetaan ruutujen määrällä, jolloin saadaan yhden ruudun eli cellin koko
+    // ja se palautetaan return -komennolla
+    return (gameBoardSize / BOARD_SIZE);
+    }
 
 function startGame(){
     console.log("works");
     document.getElementById("intro-screen").style.display='none'
     document.getElementById("game-screen").style.display='block'
     board = generateRandomBoard();
+    drawBoard(board);
 }
 function generateRandomBoard(){
     const newBoard = Array.from({length: BOARD_SIZE}, ()=> Array(BOARD_SIZE).fill(' '));
@@ -26,15 +38,22 @@ function generateRandomBoard(){
 function drawBoard(board){
     const gameBoard = document.getElementById("game-board");
 
+    const cellSize = calculateCellSize();
+
     gameBoard.style.gridTemplateColumns = `repeat(${BOARD_SIZE}, 1fr)`;
 
     for(let y = 0; y < BOARD_SIZE; y++){
         for(let x = 0; x < BOARD_SIZE; x++){
             const cell = document.createElement('div');
             cell.classList.add('cell');
+
+            cell.style.width = cellSize + "px";
+            cell.style.height = cellSize + "px";
+
             if(board[y][x]=='W'){
                 cell.classList.add('wall');
             }
+            gameBoard.appendChild(cell);
         }
     }
 
